26cbc4e1553e056cb4fed83725ca44f9568713b4
Sanjeet Dasharath <sanjeetdasharath@gmail.com>
2026-03-02 00:33:55 +1100

    extensions: gate CString import for non-wasm targets

--- src/extensions/mod.rs
+++ src/extensions/mod.rs
@@ -1,6 +1,8 @@
-use std::ffi::{CString, c_char, c_void};
+use std::ffi::{c_char, c_void};
 use std::fs;
 use std::path::{Path, PathBuf};
+#[cfg(not(target_arch = "wasm32"))]
+use std::ffi::CString;
 
 pub const PYRS_EXTENSION_MANIFEST_SUFFIX: &str = ".pyrs-ext";
 pub const PYRS_EXTENSION_ABI_TAG: &str = "pyrs314";
